1effab518a3790f37d7c171da3b9dd1fc3b0d99b
Jason Chestnut <jason.chestnut@gmail.com>
2024-11-06 19:10:43 -0500

    Fix first load time error.

--- lua/quotebook/init.lua
+++ lua/quotebook/init.lua
@@ -1,7 +1,15 @@
-local function example()
-  return true
+local M = {}
+
+function M.setup(opts)
+   opts = opts or {}
+
+   vim.keymap.set("n", "<Leader>h", function()
+      if opts.name then
+         print("hello, " .. opts.name)
+      else
+         print("hello")
+      end
+   end)
 end
 
-return {
-  example = example,
-}
+return M
--- plugin/quotebook.lua
+++ plugin/quotebook.lua
@@ -1,1 +1,3 @@
-print("pauperboards/quotebook.lua is executed!")
+if vim.fn.has("nvim-0.7.0") ~= 1 then
+   vim.api.nvim_err_writeln("Example.nvim requires at least nvim-0.7.0.")
+end
